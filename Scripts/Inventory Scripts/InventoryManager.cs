57f9c683f8a169674d2f344726e15c44e2c9026d
BenWargowski <116836124+BenWargowski@users.noreply.github.com>
2023-02-21 02:35:50 -0800

    Add files via upload
    Lotsa stuff here. Basically added a couple of other plants (glass and titanium) that work in basically the same way as fuel (but with different values, of course). I also have a basic version of a worker bot (one of the AI things I'm workin on), though its very buggy and needs a bunch of work still.

---
+++ Scripts/Inventory Scripts/InventoryManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public int maxStackedItems;
+    public InventorySlot[] inventorySlots;
+    [SerializeField] private GameObject inventoryItemPrefab;
+    [SerializeField] private GameObject mainInventory;
+
+    public bool AddItem(ItemSO itemSO)
+    {
+
+        // Stacking logic for stackable items
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null && itemInSlot.item == itemSO && itemInSlot.count < maxStackedItems && itemInSlot.item.stackable)
+            {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+        }
+
+        // Spawns 
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(itemSO, slot);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Instantiates new items in empty inventory slot
+    private void SpawnNewItem(ItemSO itemSO, InventorySlot slot)
+    {
+        GameObject newItemGameObject = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGameObject.GetComponent<InventoryItem>();
+        inventoryItem.InitializeItem(itemSO);
+    }
+
+    // Open and close main inventory with I
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && !mainInventory.gameObject.activeSelf)
+        {
+            mainInventory.gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && mainInventory.gameObject.activeSelf)
+        {
+            mainInventory.gameObject.SetActive(false);
+        }
+    }
+}
