096ccd8193780a838f3285118ddd707afbc4b39e
Jose <78896687+jv7924@users.noreply.github.com>
2023-02-28 17:11:18 -0800

    Worked on inventory scripts but not done
    Added using number keys to select which inventory slot you are in. In the process of implementing a item use feature. Still working on the old inventory scripts because the new ones were taking longer than I wanted.

--- Assets/Scripts/Inventory Scripts/InventoryManager.cs
+++ Assets/Scripts/Inventory Scripts/InventoryManager.cs
@@ -9,6 +9,24 @@ public class InventoryManager : MonoBehaviour
     [SerializeField] private GameObject inventoryItemPrefab;
     [SerializeField] private GameObject mainInventory;
 
+    private int selectedSlot = -1;
+
+    private void Start()
+    {
+        ChangeSelectedSlot(0);
+    }
+
+    private void ChangeSelectedSlot(int newValue)
+    {
+        if (selectedSlot >= 0)
+        {
+            inventorySlots[selectedSlot].Deselected();
+        }
+
+        inventorySlots[newValue].Selected();
+        selectedSlot = newValue;
+    }
+
     public bool AddItem(ItemSO itemSO)
     {
 
@@ -47,6 +65,31 @@ public class InventoryManager : MonoBehaviour
         inventoryItem.InitializeItem(itemSO);
     }
 
+    public ItemSO GetSelectedItem(bool use)
+    {
+        InventorySlot slot = inventorySlots[selectedSlot];
+        InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+        if (itemInSlot != null)
+        {
+            ItemSO item = itemInSlot.item;
+            if (use == true)
+            {
+                itemInSlot.count--;
+                if (itemInSlot.count <= 0)
+                {
+                    Destroy(itemInSlot.gameObject);
+                }
+                else
+                {
+                    itemInSlot.RefreshCount();
+                }
+            }
+            return item;
+        }
+        else
+            return null;
+    }
+
     // Open and close main inventory with I
     private void Update()
     {
@@ -57,6 +100,15 @@ public class InventoryManager : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.I) && mainInventory.gameObject.activeSelf)
         {
             mainInventory.gameObject.SetActive(false);
+        }
+
+        if (Input.inputString != null)
+        {
+            bool isNumber = int.TryParse(Input.inputString, out int number);
+            if (isNumber && number > 0 && number < 9)
+            {
+                ChangeSelectedSlot(number - 1);
+            }
         }
     }
 }
--- Assets/Scripts/Inventory Scripts/InventorySlot.cs
+++ Assets/Scripts/Inventory Scripts/InventorySlot.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    public Image image;
+    public Color selectedColor;
+    public Color notSelectedColor;
+
+    private void Awake()
+    {
+        Deselected();
+    }
+
+    public void Selected()
+    {
+        image.color = selectedColor;
+    }
+
+    public void Deselected()
+    {
+        image.color = notSelectedColor;
+    }
+
     // Allows items to be placed in empty item slots
     public void OnDrop(PointerEventData eventData)
     {
