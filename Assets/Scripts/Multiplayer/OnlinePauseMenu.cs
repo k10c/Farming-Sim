131c888780fbbdb7fb3e5b4a8f62d50311c833ac
KingBenThe1st <93746784+KingBenThe1st@users.noreply.github.com>
2023-03-19 19:58:33 -0700

    Pause Time Fix
    Added a multiplayer version of the pausemenu script, and made a slight adjustment to changeScene() so that it starts time every time it switches.

---
+++ Assets/Scripts/Multiplayer/OnlinePauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlinePauseMenu : MonoBehaviour
+{
+    
+    // variable to keep track of whether or not the game is paused; default false
+    private static bool GameIsPaused = false;
+    // 
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the player(s) press esc or p, pauses or unpauses the game dep. on curr state
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+
+    // close pause menu and unfreeze time
+    void Resume(){
+        pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+    }
+
+    // freeze time and open pause menu
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+    }
+}
--- Assets/Scripts/UI Scripts/ChangeScene.cs
+++ Assets/Scripts/UI Scripts/ChangeScene.cs
@@ -10,6 +10,7 @@ public class ChangeScene : MonoBehaviour
     // Switches to the scene in the build with the given scene ID
    public void MoveToScene(int SceneID)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneID);
     }
 }
