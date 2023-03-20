8fbf37e5495080bae4bac82b3fc7134fec79ae01
KingBenThe1st <93746784+KingBenThe1st@users.noreply.github.com>
2023-03-20 12:03:18 -0700

    Interaction Fix
    There was a bug where the player wouldn't be able to leave the ship menu after the bot spawned because it immediately interacted with the player.
    Now the player interacts with everything within their radius at once, so it should no longer be a problem.

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -8,14 +8,20 @@ public class PlayerInteract : MonoBehaviour
 {
 	[SerializeField] private string interactKey = "r";
 	public Collider2D lastTouched;
-	public InteractableType target;
+	public List<InteractableType> targets = new List<InteractableType>();
 	
 	// Update is called once per frame
     void Update()
     {
-		if(Input.GetKeyDown(interactKey) && target != null)
+		List<InteractableType> interactables = new List<InteractableType>();
+		if(Input.GetKeyDown(interactKey))
 		{
-			target.Interact(this.gameObject);
+			foreach(InteractableType target in targets)
+				if(target != null)
+					interactables.Add(target);
+			
+			foreach(InteractableType interactable in interactables)
+				interactable.Interact(this.gameObject);
 		}
     }
 	
@@ -24,17 +30,16 @@ public class PlayerInteract : MonoBehaviour
 	{
 		if(collision.gameObject.tag == "Plant" || collision.gameObject.tag == "Robot" || collision.gameObject.tag == "Ship")
 		{
-			lastTouched = collision;
-			target = (InteractableType)collision.gameObject.GetComponent(typeof(InteractableType));
+			targets.Add((InteractableType)collision.gameObject.GetComponent(typeof(InteractableType)));
 		}
 	}
 	
 	//notices if the player walks away from an interactable object
 	private void OnTriggerExit2D(Collider2D collision)
     {
-        if(lastTouched == collision)
+		if(collision.gameObject.tag == "Plant" || collision.gameObject.tag == "Robot" || collision.gameObject.tag == "Ship")
 		{
-			target = null;
+			targets.Remove((InteractableType)collision.gameObject.GetComponent(typeof(InteractableType)));
 		}
     }
 	
